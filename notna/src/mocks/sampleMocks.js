d7463177ebd4783a923dd52b49afc83514157de7
minomi <minomi@minomiui-MacBookPro.local>
2022-10-20 23:53:29 +0900

    Feat: 샘플 mock데이터를 json파일에서 가져오도록 변경

--- notna/src/mocks/sampleMocks.js
+++ notna/src/mocks/sampleMocks.js
@@ -1,10 +1,11 @@
 import { rest } from "msw";
+import totalFertilityRateData from "./json/totalFertilityRate.json";
 
 const todos = ["먹기", "자기", "놀기"];
 
 export const sampleMocks = [
   //get
   rest.get("/samples", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(todos));
+    return res(ctx.status(200), ctx.json(totalFertilityRateData));
   })
 ];
