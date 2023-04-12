8d55444134128f1e1bb648c8249a8fc708778c53
minoming.kim <minoming.kim@gmail.com>
2023-04-12 23:36:14 +0900

    Feat: RTK store, slice, service 기본 구성

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Chart from "./component/chart/Chart";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { getData } from './service/homeService'
 
 function App() {
-  const [data1, setData1] = useState();
-  const [data2, setData2] = useState();
+  const { totalFertilityRate } = useSelector((state) => state.home)
+  const dispatch = useDispatch()
 
-  useEffect(() => {
-    axios
-      .get("/liveBirth", {
-        params: {
-          area: "00",
-          sub: "10",
-          startYear: "201901",
-          endYear: "202201"
-        }
-      })
-      .then((res) => {
-        const _data1 = [];
-        const _data2 = [];
-        res.data.data.map((item) => {
-          _data1.push(item["PRD_DE"]);
-          _data2.push(item["DT"]);
-
-          setData1(_data1);
-          setData2(_data2);
-        });
-      })
-      .catch((e) => {
-        alert("error");
-      });
-  }, []);
+  const handle = () => {
+    dispatch(getData())
+  }
 
   return (
-    <div>
-      <Chart data2={data1} data1={data2}></Chart>
-    </div>
-  );
+    <>
+      <button onClick={handle}>getData</button>
+      <div>{totalFertilityRate}</div>;
+    </>
+  )
 }
 
-export default App;
+export default App
